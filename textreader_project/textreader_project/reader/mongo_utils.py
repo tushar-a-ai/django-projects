d57bfe50fd20791f926d5adea0857bc7aa6f72c2
tushar-a-ai <83804989+tushar-a-ai@users.noreply.github.com>
2025-05-28 13:39:22 +0545

    Update mongo_utils.py

--- textreader_project/textreader_project/reader/mongo_utils.py
+++ textreader_project/textreader_project/reader/mongo_utils.py
@@ -2,6 +2,6 @@ from pymongo import MongoClient
 
 
 def get_database():
-    CONNECTION_STRING = "mongodb+srv://tusharkunwar993:gotchmall2058@cluster0.nne9ttn.mongodb.net/file_demo?retryWrites=true&w=majority"
+    CONNECTION_STRING = ""
     client = MongoClient(CONNECTION_STRING)
     return client["file_demo"]
